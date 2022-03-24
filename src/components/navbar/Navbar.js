f3117506ab970ee4a9631a1ef4fc57dce7b7ca80
Jajuan Jones <jajuanojones2@gmail.com>
2022-03-24 12:19:09 -0500

    non-working changes to navbar inital algorithm

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,26 +2,53 @@ import React from "react"
 import { Link } from "react-router-dom"
 import logo from "./navlogo2.png"
 import "./Navbar.css"
+import userEvent from "@testing-library/user-event"
 
 export const Navbar = () => {
     //Links for browser components
+    const checkCurrentAuthOfUser = () => {
+        return fetch(`http://localhost:8088/users?userId=${parseInt(localStorage.getItem("musilink_user"))}`)
+            .then(res => res.json())
+    }
+    const NavbarForMemberOrUser = (user) => {
+        checkCurrentAuthOfUser()
+        .then((user) => {
+            if(user.isMember) {
+                return <ul className="navbar">
+                    <div className="navLogo">
+                        <img src={logo}></img>
+                    </div>
+                    <div className="navbar_item active">
+                        <Link className="navbar_link" to="/memberRequests">Your Requests</Link>
+                    </div>
+                    <div className="navbar_item active">
+                        <Link to="/" onClick={()=>{
+                            localStorage.removeItem("musilink_user")}}>Logout</Link>
+                    </div>
+                    </ul>         
+            }else if(!user.isMember) {
+                return  <ul className="navbar">
+                    <div className="navLogo">
+                        <img src={logo}></img>
+                    </div>
+                    <div className="navbar_item active">
+                        <Link className="navbar_link" to="/members">Find a Member</Link>
+                    </div>
+                    <div className="navbar_item active">
+                        <Link className="navbar_link" to="/userRequests">Your Requests</Link>
+                    </div>
+                    <div className="navbar_item active">
+                        <Link to="/" onClick={()=>{
+                            localStorage.removeItem("musilink_user")}}>Logout</Link>
+                    </div>
+                    </ul>
+            }
+        })
+    }
+
     return(
         <>
-        <ul className="navbar">
-            <div className="navLogo">
-                <img src={logo}></img>
-            </div>
-            <div className="navbar_item active">
-                <Link className="navbar_link" to="/members">Find a Member</Link>
-            </div>
-            <div className="navbar_item active">
-                <Link className="navbar_link" to="/userRequests">Your Requests</Link>
-            </div>
-            <div className="navbar_item active">
-                <Link to="/" onClick={()=>{
-                    localStorage.removeItem("musilink_user")}}>Logout</Link>
-            </div>
-        </ul>
+            <NavbarForMemberOrUser/>
         </>
     )
 }
